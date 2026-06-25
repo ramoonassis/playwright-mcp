80eda8b93c48ee80a7a07e775e9b9e27a15834f0
Ramon Assis <ramoonassis@gmail.com>
2026-06-25 10:55:59 -0300

    creation of test CT001 with sdet-automator.prompt

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
  * Read environment variables from file.
@@ -28,7 +32,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || "https://www.kitchenaid.com.br/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot: "only-on-failure",
--- tests/e2e/test_home.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('home @e2e', async ({ page }) => {
-  await page.goto('https://testbeyond.com');
-
-  await expect(page).toHaveTitle('Projeto TestBeyond');
-});
